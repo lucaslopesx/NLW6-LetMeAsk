411b80f02d709003f5f6a2f3e0a980d8b361d882
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-08-23 12:36:16 -0300

    Update Button.tsx

--- letmeask/src/components/Button.tsx
+++ letmeask/src/components/Button.tsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
 
 export function Button() {
-  const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
 
-  function increment() {
-    setCounter(counter + 1);
-  }
+    function increment() {
+        setCounter(counter + 2);
+    }
 
-  return (
-    <button onClick={increment}>
-      {counter}
-    </button>
-  )
+    return <button onClick={increment}>{counter}</button>;
 }
